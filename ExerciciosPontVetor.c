2c8f28b0953a2b43f270f4b836dffc38e26cd1d1
alekastrup <alexandrekaprestes@gmail.com>
2026-09-11 16:33:25 -0300

    Exercicio Ponteiro e Vetor

---
+++ ExerciciosPontVetor.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*void calculaOperacao(int a, int b, int* soma, int* produto, int* dif)
+{
+	*soma = a + b;
+	*dif = a - b;
+	*produto = a * b;
+}
+
+int main(void)
+{
+	int a,b, soma, produto, dif;
+	printf("Digite 2 numeros: ");
+	scanf("%d %d", &a, &b);
+	calculaOperacao(a, b, &soma, &produto, &dif);
+	printf("\nsoma: %d\nprodtuo: %d\ndif: %d\n", soma, produto, dif);
+	return 0;
+}*/
+
+/*void calcularMedia()
+{
+	float nota;
+	int matr;
+	float media = 0;
+	float notas[3];
+	int matr_list[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		scanf("%d %f", &matr, &nota);
+		media += nota;
+		notas[i] = nota;
+		matr_list[i] = matr;
+	}
+	media = media / 3;
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (notas[i] > media)
+		{
+			printf("%.1f\n", notas[i]);
+		}
+	}
+
+}
+
+int main(void) {
+
+	calcularMedia();
+	return 0;
+}*/
